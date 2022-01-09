c1bccf5f6f140bb42277a3ffb8258021bde3951a
JaredPetersen2003 <jared.petersen9@gmail.com>
2022-01-09 23:07:44 +0200

    added fullsceen nav

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-$(document).ready(function () {
-	var myFullpage = new fullpage('#fullpage', {
-		//options here
-		autoScrolling: true,
-		scrollHorizontally: true
-	});
+$(document).ready(function(){
+	isOpen = false;
+	console.log(isOpen);
 });
 
 //methods
 fullpage_api.setAllowScrolling(false);
+
+var isOpen;
+
+
+function navClick(){
+	console.log(isOpen);
+	if (isOpen == false){
+		openNav();
+		isOpen = true;
+	}
+	else{
+		closeNav();
+		isOpen = false;
+	}
+}
+
+function linkClick(){
+		$('.false-check').prop('checked', false);
+		navClick();
+
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+	document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+  }
