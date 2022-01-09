01f491cbeb15473c4fca68b085d678365aacbd26
JaredPetersen2003 <jared.petersen9@gmail.com>
2022-01-09 15:58:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	var myFullpage = new fullpage('#fullpage', {
+		//options here
+		autoScrolling: true,
+		scrollHorizontally: true
+	});
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
